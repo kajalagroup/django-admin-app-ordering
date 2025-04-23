4c8b226272a2963d64d51405c0945f2cb99d24d1
Tuan Bach Van <bachvtuan@gmail.com>
2025-04-23 14:33:05 +0700

    Remove environment

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="app_ordering",
-    version="1.8",
+    version="1.9",
     author=u"Tuan Bach Van",
     author_email="tuan@kajala.com",
     packages=find_packages(exclude=["project", "venv"]),
